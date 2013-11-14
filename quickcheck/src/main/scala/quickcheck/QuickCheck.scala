894420e276c45f66c2b00708bb1bad2e695aad96
swarkentin <sheldon.warkentin@gmail.com>
2013-11-13 23:23:37 -0700

    Completed adding tests

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -30,26 +30,55 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   property("minOfMeldIsOneOrOther") = forAll { (h1: H, h2: H) =>
     val melded = meld(h1, h2)
-    (findMin(melded) == findMin(h1)) ||
-      (findMin(melded) == findMin(h2))
+    val minH1 = findMin(h1)
+    val minH2 = findMin(h2)
+    val minMelded = findMin(melded)
+    if (minH1 < minH2) minMelded == minH1 else minMelded == minH2
   }
 
   property("heapIsOrderedWhileDeleting") = forAll { (h: H) =>
-    findMin(h) == findMin(findAllMin(h))
+    if (isEmpty(h))
+      true
+    else
+      findAllMin(h, findMin(h))
   }
 
-  //Returns the heap passed in if and only if the it is empty, or its mimimum
+  property("heapMeldIsOrderedWhileDeleting") = forAll { (h1: H, h2: H) =>
+    val melded = meld(h1, h2)
+    if (isEmpty(melded))
+      true
+    else
+      findAllMin(melded, findMin(melded))
+  }
+
+  property("heapMeldReturnsOrderedSequence") = {
+    val h1 = insert(-1, insert(0, insert(1, empty)))
+    val h2 = insert(-2, insert(10, empty))
+    val h3 = meld(h1, h2)
+    checkEach(h3, List(-2, -1, 0, 1, 10))
+  }
+
+  def checkEach(h: H, list: List[Int]): Boolean = {
+    isEmpty(h) && list.isEmpty ||
+      (!isEmpty(h) &&
+        findMin(h) == list.head &&
+        checkEach(deleteMin(h), list.tail))
+  }
+
+  //Returns the heap passed in if and only if the it is empty, or its minimum
   //is less than the minimum of each heap following the deletion of the minimum.
-  def findAllMin(h: H): H = {
-    if (!isEmpty(h)) {
-      val min = findMin(h)
-      val remainder = deleteMin(h)
-      if (isEmpty(remainder) || ord.lteq(min, findMin(remainder)))
-        h
-      else
-        ???
-    }
-    h
+  def findAllMin(h: H, i: Int): Boolean = {
+    val remainder = deleteMin(h)
+    isEmpty(remainder) ||
+      (ord.lteq(i, findMin(remainder)) && findAllMin(remainder, findMin(remainder)))
+
+  }
+
+  property("meldHeapsAndDelete") = forAll { (h1: H, h2: H) =>
+    val h = meld(h1, h2)
+    val min1 = findMin(h1)
+    val min2 = findMin(h2)
+    if (min1 < min2) findMin(deleteMin(h)) <= min2 else findMin(deleteMin(h)) <= min1
   }
 
   property("gen1") = forAll { (h: H) =>
